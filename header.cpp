552e255f41985e2fc2b3971f482a1cf98687add7
KIMIA <kimiabayat16@gnail.com>
2024-03-09 13:57:15 +0330

    update Health class

--- header.cpp
+++ header.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
 #include<string>
-#include <algorithm>
 #include<vector>
 #include<algorithm>
 #include "header.h"
 using namespace std;
 
 // Function And Class Method Decleration
-
 
     Health ::Health() : maxHealth(100),currentHealth(100){}
     int Health::getCurrentHealth(){
@@ -18,14 +16,14 @@ using namespace std;
     }
     void Health::decreaseHealth(int damage){
         currentHealth -= damage;
-        currentHealth = max(0, min(currentHealth,maxHealth));
+        if(currentHealth <= 0)
+            currentHealth = 0;
     }
     void Health::increaseHealth (int amount){
         currentHealth += amount;
-        currentHealth = min(maxHealth,currentHealth); 
+        if(currentHealth >= maxHealth)
+            currentHealth = maxHealth;
     }
-
-
 
 Stamina ::Stamina(int val , int max) : value(val) , maximum(max) {}
 int Stamina ::getValue() const {
@@ -47,15 +45,17 @@ void Stamina ::increase(int amount) {
 int Stamina ::getMaximum() const {
     return maximum;
 }
+    
 
 Experience::Experience(int Level):level(Level),maximum(100+(Level*50)),currentValue(0){};
 
 void Experience::setMaximum(){
     maximum=100+(level*50);
 }
+
 void Experience::setCurrentValue(int selfDamage,int enemyDamage,int usedStamina){
     currentValue+=(0.5*selfDamage)+(0.2*enemyDamage)+(0.3*usedStamina);
     if(currentValue>=maximum){
         currentValue=0;
     }
-}    
+}
