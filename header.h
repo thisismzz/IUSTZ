e2fdf1d0ad11916c46fd5df71c39ec515e1fa300
KIMIA <kimiabayat16@gnail.com>
2024-03-16 13:31:28 +0330

    working on shopmenu and showing different items

--- header.h
+++ header.h
@@ -6,6 +6,8 @@
 #include<algorithm>
 #include <thread>
 #include <chrono>
+#include <conio.h> 
+#include <cstdlib>
 
 using namespace std;
 // *----------------------------------------------------------------*
@@ -337,6 +339,80 @@ vector <Food*> Food::shop_items_food;
 // *----------------------------------------------------------------*
 
 void printWithDelay() {}
+
+// *----------------------------------------------------------------*
+// *----------------------------------------------------------------*
+// *----------------------------------------------------------------*
+
+void ShopMenu(){
+    while(true){
+        system("cls");
+        int number;
+        cout << "You enter the shop." << endl << "What do you want to buy?" << endl 
+        << "[1].Permanent Items" << endl << "[2].Throwable Items" << endl << "[3].Consumable Items" << endl << "[4].Exit" << endl;
+        cin >> number;
+        switch(number){
+            case 1: Show_Permanent_Items();
+                break;
+        
+            case 2: Show_Throwable_Items();
+                break;
+        
+            case 3: Show_Consumable_Items();
+                break;
+        
+            case 4: 
+            cout << "You left the shop." << endl;
+                return;
+
+            default: 
+            cout << "Wrong number!" << endl << "Press enter to continue";
+            getch();
+                break;
+        }
+    }
+}
+
+void Show_Permanent_Items(){
+    system("cls");
+    int number;
+    WarmWeapon warmweapon;
+    ColdWeapon coldweapon;
+    cout << "[1].WarmWeapons" << endl << "[2].ColdWeapons" << endl << "[3].back" << endl;
+    switch(number){
+        case 1: warmweapon.showItems();
+            break;
+        
+        case 2: coldweapon.showItems();
+            break;
+        
+        case 3: 
+            return;
+    }
+}
+void Show_Throwable_Items(){
+    system("cls");
+    Throwable throwable;
+    throwable.showItems();
+}
+
+void Show_Consumable_Items(){
+    system("cls");
+    int number;
+    Medicine medicine;
+    Food food;
+    cout << "[1].Medicines" << endl << "[2].Foods" << endl << "[3].back" << endl;
+    switch(number){
+        case 1: medicine.showItems();
+            break;
+        
+        case 2: food.showItems();
+            break;
+        
+        case 3: 
+            return;
+    }
+}
 
 // *----------------------------------------------------------------*
 // *----------------------------------------------------------------*
