7e6cd5a287e06223e1c4b73535fa5cecb2a1a226
KIMIA <149102812+KimiaBayat@users.noreply.github.com>
2024-04-11 02:07:13 +0330

    some changes in health and stamina class for maximum part

--- header.cpp
+++ header.cpp
@@ -43,7 +43,7 @@ vector <Food> Food::shop_items_food={};
 // *----------------------------------------------------------------*
 // *----------------------------------------------------------------*
 
-Health::Health(int l=1) : maxHealth(100+(50*(l-1))) , currentHealth(100) {} // Constructor
+Health::Health(int l=1) : maxHealth((l-1)*100) , currentHealth(100) {} // Constructor
 
 int Health::getCurrentHealth() {
     return currentHealth; // Returns current health
@@ -62,19 +62,19 @@ void Health::decreaseHealth(int damage){
 
 void Health::increaseHealth (int amount){
     currentHealth += amount; // Increases health
-    cout<<"HEALTH INCREASED FOR "<< amount << " HP\n";
+    cout<<"HEALTH INCREASED FOR "<< amount << " HP!\n";
     if(currentHealth >= maxHealth)
         currentHealth = maxHealth; // Ensures health doesn't exceed maxHealth
 }
 
 void Health::updateMaxHealth(){
-    maxHealth+=50;
+    maxHealth+=100;
 }
 
 // *----------------------------------------------------------------*
 // *----------------------------------------------------------------*
 
-Stamina::Stamina(int cs,int l=1):maximum(100+(50*(l-1))),currentStamina(cs){} // Constructor
+Stamina::Stamina(int cs,int l=1):maximum((l-1)*100),currentStamina(cs){} // Constructor
 
 void Stamina::decreaseStamina(int amount) {
     currentStamina -= amount; // Decreases stamina
@@ -91,7 +91,7 @@ void Stamina::increaseStamina(int amount) {
 }
 
 void Stamina::updateMaximumStamina(){
-    maximum+=50; // Increases maximum stamina
+    maximum+=100; // Increases maximum stamina
 }
 
 int Stamina::getCurrentStamina(){
@@ -436,8 +436,6 @@ void Backpack::ConsumeMedForSurvival() {
         auto iter = next(MedicineItems.begin(), choice - 1);
         Medicine* medicine = new Medicine(iter->first);
         medicine->use(*player);
-        cout << "YOU USED " << medicine->getName() << " ITEM." << endl;
-        getch();  // Wait for a key press
         player->newLife();   //change the state from DEFEATED to ALIVE
         playground();
     }
@@ -472,8 +470,6 @@ void Backpack::ConsumeFoodForSurvival() {
         auto iter = next(FoodItems.begin(), choice - 1);
         Food* food = new Food(iter->first);
         food->use(*player);
-        cout << "YOU USED " << food->getName() << " ITEM." << endl;
-        getch();  // Wait for a key press
         player->newLife();   //change the state from DEFEATED to ALIVE
         BattleMenu();
     }
@@ -1692,10 +1688,10 @@ void Food::addToVectors(){
 
 void Food::use(Human& human){
     if(human.stamina.getCurrentStamina() < human.stamina.getMaxStamina()){
+        cout << "FOOD CONSUME SUCCESSFULLY." << endl;
         human.stamina.increaseStamina(strength); // Increases the human's stamina
         Backpack *b=human.getBackpack();
         b->useFoodItemCount(*this); // Uses a food item from the human's backpack
-        cout << "FOOD CONSUME SUCCESSFULLY." << endl;
         getch();
     }
     else{
@@ -2270,7 +2266,6 @@ void goodbye(){
     system("cls");
     if(player->getState()==PlayerState::DEFEATED){
         gameOVER();
-        Sleep(3000);
         exit(0);
     }
     else{
@@ -2304,7 +2299,7 @@ void battleGround_humanEnemy(){
         else{
         //enemy turn
 
-            cout<<"\nENEMY'S TURN:\n";
+            cout<<"ENEMY'S TURN:\n";
             Enemy.updateState();
             Enemy.decision();
             Enemy.showInfo();
@@ -2316,22 +2311,24 @@ void battleGround_humanEnemy(){
         system("cls");
     }
 
-    if (player->getState()==PlayerState::ALIVE) {
-        cout<<"CONGRATULATIONS!! \nYOU HAVE WON THE MATCH. \nTHE FOLLOWING ITEM'S ITEMS WILL ADD TO YOUR BACKPACK: \n";
-        
+    if (player->getState()==PlayerState::ALIVE){
+        cout<<"CONGRATULATIONS!! \nYOU HAVE WON THE MATCH.\n"; 
+        player->getBankAccount()->prize(player->getLevel()*1000);
+        getch();
+
+        system("cls");
+        cout << "THE FOLLOWING ITEMS WILL ADD TO YOUR BACKPACK: \n";
+
         //show Enemy's item
         //method to add to player backpack;
         Enemy.showBackpackItems();
-        Enemy.transferItems();
-
-        player->getBankAccount()->prize(player->getLevel()*1000);
-        
+        Enemy.transferItems();  
         getch();
-            }
+    }
     else{
         cout<<"YOU HAVE LOST THIS MATCH!\n";
         getch();
-            }
+    }
     playground();
 }
 
@@ -2351,7 +2348,7 @@ void battleGround_basicZombie(){
         }
         else{
         //enemy turn
-            cout<<"\nENEMY'S TURN:\n";
+            cout<<"ENEMY'S TURN:\n";
             Enemy.bite();
 			//Enemy.updateState();
             Enemy.showInfo();
@@ -2367,11 +2364,11 @@ void battleGround_basicZombie(){
         cout<<"CONGRATULATIONS!! \nYOU HAVE WON THE MATCH.\n";
         player->getBankAccount()->prize(player->getLevel()*500);
         getch();
-            }
+    }
     else{
         cout<<"YOU HAVE LOST THIS MATCH!\n";
         getch();
-            }
+    }
     playground();
 }
 
@@ -2407,11 +2404,11 @@ void battleGround_advZombie(){
         cout<<"CONGRATULATIONS!! \nYOU HAVE WON THE MATCH.\n";
         player->getBankAccount()->prize(player->getLevel()*800);
         getch();
-            }
+    }
     else{
         cout<<"YOU HAVE LOST THIS MATCH!\n";
         getch();
-            } 
+    } 
     playground();
 }
 
@@ -2424,7 +2421,7 @@ void BattleMenu() {
     int number;
     cout << "What do you want to do? (Attack ends your turn.)" << endl
          << "[1].ATTACK" << endl << "[2].CONSUME FOOD OR MEDICINE" << endl << "[3].SHOW PLAYER'S INFO" << endl
-        << "[4].SHOW BACKPACK" << endl << "[5].UPGRADE WEAPON'S SKILL"<< endl <<"[6].QUIT"<< endl;
+         << "[4].SHOW BACKPACK" << endl << "[5].UPGRADE WEAPON'S SKILL"<< endl <<"[6].QUIT"<< endl;
     cin >> number;
 
     string temp;
